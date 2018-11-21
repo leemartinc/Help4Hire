85a3432142c9b72b0f9c9504da586998fd69acdc
Mohamed Ali <mali30@student.gsu.edu>
2018-11-21 15:29:10 -0500

    UserProfile Activity and XML

---
+++ app/src/main/java/losdos/help4hire/UserProfileActivity.java
@@ -0,0 +1,37 @@
+package losdos.help4hire;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+
+/*
+    This class is for the user profile activity. User enters fname, lname and
+    address and saves it. This class will store all of that information into
+    firebase
+
+ */
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
