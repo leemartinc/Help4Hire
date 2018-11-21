44639f588c416fbbe674d399c3e94a491fa8c5fd
Mohamed Ali <mali30@student.gsu.edu>
2018-11-21 15:25:34 -0500

    Created ProviderSignUp class

---
+++ app/src/main/java/losdos/help4hire/ProviderSignUp.java
@@ -0,0 +1,19 @@
+package losdos.help4hire;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ProviderSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.provider_signup);
+
+
+
+}
