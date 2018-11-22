00fd04f33bbf20af197db26b1e9f4fd148951a59
ptstory <perry.t.story@gmail.com>
2018-11-22 11:13:21 -0500

    reviews are saved to database

--- app/src/main/java/losdos/help4hire/ReviewActivity.java
+++ app/src/main/java/losdos/help4hire/ReviewActivity.java
@@ -2,6 +2,7 @@ package losdos.help4hire;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,7 +10,22 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ReviewActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference ref = db.collection("reviews");
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +42,25 @@ public class ReviewActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
+                Map<String, Object> data = new HashMap<>();
+
+
                 if (ratingBar.getRating() == 0)  {
                     Toast.makeText(ReviewActivity.this, "Please include a rating.", Toast.LENGTH_LONG).show();
                 } else {
                     //save review data to database
                     rating = ratingBar.getRating();
                     review = etReview.getText().toString();
+
+
+                    data.put("review", review);
+                    data.put("review date", FieldValue.serverTimestamp());
+                    data.put("review rating", rating);
+                    data.put("reviewee", "");
+                    data.put("reviewer", user.getEmail()); //can get other identifiers such as UUID
+
+                    data.put("service name", "");
+                    ref.add(data);
 
                     //reset view to empty state
                     etReview.setText("");
