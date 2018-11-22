5e84a806760189f4ecb0f9f5764e7c67ef9ea5ba
Mohamed Ali <mali30@student.gsu.edu>
2018-11-22 14:38:18 -0500

    Fixed Issue with build.gradle and minor cosmetic changes

--- app/src/main/java/losdos/help4hire/ProviderSignUp.java
+++ app/src/main/java/losdos/help4hire/ProviderSignUp.java
@@ -1,6 +1,5 @@
 package losdos.help4hire;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,42 +12,8 @@ public class ProviderSignUp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.provier_signup);
+        setContentView(R.layout.provider_signup);
 
 
-        //add input for email address and password and send that through authentication
-        //so that we can add them as a user
-        //using the method below
-        /*
-        mAuth.createUserWithEmailAndPassword(email, password)
-        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success");
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    updateUI(user);
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                    Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show();
-                    updateUI(null);
-                }
-
-                // ...
-            }
-        });
-         */
-
-
-    }
-
-    @Override
-    public void onBackPressed() {
-        startActivity(new Intent(ProviderSignUp.this,PreSignUp.class));
-        finish();
     }
 }
-
