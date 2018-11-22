0d3cb47e4b59f2d0e4f3711234bb32b47b67c2e9
Mohamed Ali <mali30@student.gsu.edu>
2018-11-22 16:53:34 -0500

    Fixed the layout for ProviderSignUp and Added Spinner

--- app/src/main/java/losdos/help4hire/ProviderSignUp.java
+++ app/src/main/java/losdos/help4hire/ProviderSignUp.java
@@ -1,13 +1,18 @@
 package losdos.help4hire;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class ProviderSignUp extends AppCompatActivity {
+public class ProviderSignUp extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +20,61 @@ public class ProviderSignUp extends AppCompatActivity {
         setContentView(R.layout.provider_signup);
 
 
+        Spinner spinner = findViewById(R.id.mySpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.provider_choices, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
+
+        //add input for email address and password and send that through authentication
+        //so that we can add them as a user
+        //using the method below
+        /*
+        mAuth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+
+                // ...
+            }
+        });
+         */
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(ProviderSignUp.this,PreSignUp.class));
+        finish();
+    }
+
+    // These two methods below are for the spinner in the ProviderSignUp
+    @Override
+    // Will show a toast message after user selects spinner item
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        String text = adapterView.getItemAtPosition(position).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
+
