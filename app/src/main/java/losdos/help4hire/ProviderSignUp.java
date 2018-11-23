7081138e12cc1287927f5e2d21bcc80304b85119
Mohamed Ali <mali30@student.gsu.edu>
2018-11-22 21:25:50 -0500

    Can Post to FireStore but needs minor fixes and Fixed Layout Again

--- app/src/main/java/losdos/help4hire/ProviderSignUp.java
+++ app/src/main/java/losdos/help4hire/ProviderSignUp.java
@@ -2,17 +2,58 @@ package losdos.help4hire;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.dynamic.ObjectWrapper;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProviderSignUp extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private static final String TAG = "MainActivity";
+    private static final String KEY_FNAME = "First Name";
+    private static final String KEY_LNAME = "Last Name";
+    private static final String KEY_ADDRESS = "Address";
+    private static final String KEY_SPINNER_VALUE = "Spinner Value";
+    private static final String KEY_FIXED= "Fixed Rate";
+    private static final String KEY_HOURLY = "Hourly Rate";
+    private static final String KEY_AGE_VALE= "Age Value";
+    private static final String KEY_DOLLAR_VALUE = "Dollar Value";
+    private static final String KEY_YES_INSURED = "Yes Insured";
+    private static final String KEY_NO_INSURED = "No Insured";
+
+
+    private EditText fName;
+    private EditText lName;
+    private EditText address;
+    private Spinner my_spinner;
+    private RadioButton fixedRadioButton;
+    private RadioButton hourlyRadioButton;
+    private EditText ageEditText;
+    private EditText dollarEditText;
+    private RadioButton yesButton;
+    private RadioButton noButton;
+
+
+    // Reference to firestore database
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +68,18 @@ public class ProviderSignUp extends AppCompatActivity implements AdapterView.OnI
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
 
+        // FireStore Storage for Provider
+
+        fName = findViewById(R.id.firstName);
+        lName = findViewById(R.id.lastName);
+        address = findViewById(R.id.Address);
+        my_spinner = (Spinner)findViewById(R.id.mySpinner);
+        fixedRadioButton = findViewById(R.id.fixedRadioButton);
+        hourlyRadioButton = findViewById(R.id.hourlyRadioButton);
+        ageEditText = findViewById(R.id.ageEditText);
+        dollarEditText = findViewById(R.id.dollarEditText);
+        yesButton = findViewById(R.id.yesRadioButton);
+        noButton = findViewById(R.id.noRadioButton);
 
 
         //add input for email address and password and send that through authentication
@@ -75,6 +128,71 @@ public class ProviderSignUp extends AppCompatActivity implements AdapterView.OnI
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    // This method will save into firestore
+    public void saveInfo(View view){
+
+//        fName = findViewById(R.id.firstName);
+//        lName = findViewById(R.id.lastName);
+//        address = findViewById(R.id.Address);
+//        spinner = findViewById(R.id.mySpinner);
+//        fixedRadioButton = findViewById(R.id.fixedRadioButton);
+//        hourlyRadioButton = findViewById(R.id.hourlyRadioButton);
+//        ageEditText = findViewById(R.id.ageEditText);
+//        dollarEditText = findViewById(R.id.dollarEditText);
+//        yesButton = findViewById(R.id.yesRadioButton);
+//        noButton = findViewById(R.id.noRadioButton);
+
+//
+//        private static final String TAG = "MainActivity";
+//        private static final String KEY_FNAME = "First Name";
+//        private static final String KEY_LNAME = "Last Name";
+//        private static final String KEY_ADDRESS = "Address";
+//        private static final String KEY_SPINNER_VALUE = "Spinner Value";
+//        private static final String KEY_FIXED= "Fixed Rate";
+//        private static final String KEY_HOURLY = "Hourly Rate";
+//        private static final String KEY_AGE_VALE= "Age Value";
+//        private static final String KEY_DOLLAR_VALUE = "Dollar Value";
+//        private static final String KEY_YES_INSURED = "Yes Insured";
+//        private static final String KEY_NO_INSURED = "No Insured";
+
+        String fname = fName.getText().toString();
+        String lname = lName.getText().toString();
+        String my_address = address.getText().toString();
+        String spinner = my_spinner.getSelectedItem().toString();
+        String fixed_radioButton = fixedRadioButton.getText().toString();
+        String hourly_Radiobutton = hourlyRadioButton.getText().toString();
+        String age = ageEditText.getText().toString();
+        String dollar = dollarEditText.getText().toString();
+        String yes_button = yesButton.getText().toString();
+        String no_button = noButton.getText().toString();
+
+        Map<String,Object> myMap = new HashMap<String,Object>();
+        myMap.put(KEY_FNAME,fname);
+        myMap.put(KEY_LNAME,lname);
+        myMap.put(KEY_ADDRESS, my_address);
+        myMap.put(KEY_FIXED, fixed_radioButton);
+        myMap.put(KEY_HOURLY, hourly_Radiobutton);
+        myMap.put(KEY_AGE_VALE, age);
+        myMap.put(KEY_DOLLAR_VALUE, dollar);
+        myMap.put(KEY_YES_INSURED, yes_button);
+        myMap.put(KEY_NO_INSURED, no_button);
+        myMap.put(KEY_SPINNER_VALUE , spinner);
+
+        db.collection("demoProviders").document("First Provider")
+                .set(myMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(ProviderSignUp.this, "User Saved", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ProviderSignUp.this, "Error!", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, e.toString());
+            }
+        });
     }
 }
 
