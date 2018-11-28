651171167390c607204994e0b2f1395d4116bb63
5aleh <nkwabosaleh2007@gmail.com>
2018-11-28 15:13:52 -0500

    Messages onclick works now. The layout still needs to look pretty

--- app/src/main/java/losdos/help4hire/Messages.java
+++ app/src/main/java/losdos/help4hire/Messages.java
@@ -11,7 +11,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class Messages extends Fragment {
 
@@ -21,20 +24,22 @@ public class Messages extends Fragment {
     private RecyclerView rview;
     private RecyclerView.Adapter rviewadap;
 
-    public ArrayList<MessagesModel> initMessages() {
-        ArrayList<MessagesModel> mm = new ArrayList<>();
-        mm.add(new MessagesModel("Hello this is a test", "12:41"));
-        mm.add(new MessagesModel("This is another test", "12:43"));
-        mm.add(new MessagesModel("How are you doing today", "12:43"));
-        return mm;
-    }
+//    public ArrayList<MessagesModel> initMessages() {
+//        ArrayList<MessagesModel> mm = new ArrayList<>();
+//        mm.add(new MessagesModel("Hello this is a test", "12:41"));
+//        mm.add(new MessagesModel("This is another test", "12:43"));
+//        mm.add(new MessagesModel("How are you doing today", "12:43"));
+//        return mm;
+//    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         myView = inflater.inflate(R.layout.activity_messages, container, false);
 //        setContentView(R.layout.activity_messages);
 
-        ArrayList<MessagesModel> mm = initMessages();
+//        ArrayList<MessagesModel> mm = initMessages();
+        final ArrayList<MessagesModel> mm = new ArrayList<>();
+        final SimpleDateFormat sdf = new SimpleDateFormat("h:mm a", Locale.US);
 
         this.rview = myView.findViewById(R.id.reyclerview_message_list);
         RecyclerView.LayoutManager layout = new LinearLayoutManager(this.getActivity());
@@ -43,15 +48,16 @@ public class Messages extends Fragment {
         rviewadap = new MessagesAdapter(mm);
         this.rview.setAdapter(rviewadap);
 
-//        btn = myView.findViewById(R.id.button_chatbox_send);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                txtview = myView.findViewById(R.id.edittext_chatbox);
+        btn = myView.findViewById(R.id.button_chatbox_send);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtview = myView.findViewById(R.id.edittext_chatbox);
 //                rview = myView.findViewById(R.id.reyclerview_message_list);
 //                txtview.getText();
-//            }
-//        });
+                mm.add(new MessagesModel(txtview.getText().toString(), "" + sdf.format(System.currentTimeMillis())));
+            }
+        });
         return myView;
     }
 }
